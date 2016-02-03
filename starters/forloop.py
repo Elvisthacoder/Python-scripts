af37ad1b91539e111449ea09c2e780ca86c757ef
Elvisthacoder <ekk213@gmail.com>
2016-02-03 11:27:30 +0300

    reading from files

---
+++ starters/forloop.py
@@ -0,0 +1,12 @@
+'''
+Created on Feb 3, 2016
+
+@author: taifa
+'''
+#read a line from a text file
+#reasons why python is good
+
+now = open("forloop.txt")
+for line in now.readlines():
+    print(line)
+    
---
+++ starters/loops.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 3, 2016
+
+@author: taifa
+'''
+#a simple fibonacci series
+#the sum of two elements defines the next set
+
+a, b = 0,1
+while b < 50:
+    print(b)
+    a, b = b, a + b
+print("fibonacci series b < 50")
+    
+
+
+
+
+
